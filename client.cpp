2bc1e72657c18a503db419ec0be2a7f68794e453
Rares75 <rarescabalau62@gmail.com>
2025-12-03 23:46:56 +0200

    adding the client implementation and make the server to accept more inputs

--- client.cpp
+++ client.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
 
 int main()
 {
+    signal(SIGINT, SIG_DFL); // close the terminal in case you press ctrl+C
 
+    std::string SendMessage;
+    std::string RecivedMessage;
+    sockaddr_in client;
+
+    int sd;
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("socket error");
+        return errno;
+    }
+
+    client.sin_family = AF_INET;
+    client.sin_port = htons(PORT);
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if (connect(sd, (struct sockaddr *)&client, sizeof(struct sockaddr)) == -1)
+    {
+        perror("connection failed");
+        return errno;
+    }
+    while (1)
+    {
+        std::getline(std::cin, SendMessage);
+        size_t writeBytes = write(sd, SendMessage.data(), SendMessage.size());
+        if (writeBytes < 0)
+        {
+            perror("couldn't send the message to the server");
+        }
+        SendMessage.clear();
+        RecivedMessage.resize(1024);
+        size_t readBytes = read(sd, RecivedMessage.data(), RecivedMessage.size());
+        if (readBytes < 0)
+        {
+            perror("couldn't read the message from the server");
+        }
+        else
+        {
+            RecivedMessage.resize(readBytes);
+            std::cout << RecivedMessage << std::endl;
+        }
+
+        RecivedMessage.clear();
+    }
+    close(sd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -8,7 +8,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <signal.h>
-// TODO:put everthing in romanian at the end
+// TODO: put everything in romanian at the end
 #define PORT 8080
 /*
 struct sockaddr_in {
@@ -21,18 +21,25 @@ struct sockaddr_in {
 
 int main()
 {
+    // initialize all the data we need
     int sd;
     std::string MessageFromClient;
     std::string MessageToClient;
     struct sockaddr_in server;
     struct sockaddr_in from;
+    int client;
+    unsigned int length = sizeof(from);
+
+    signal(SIGINT, SIG_DFL); // close the server if you press ctrl+c
+
     // create the connection with the server
     if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
         perror("Error at opening server\n");
         return errno;
     }
-    // intialize the sockaddr_in struct
+    std::cout << "the server has turned on" << std::endl;
+    // initialize the sockaddr_in struct
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
     server.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -40,7 +47,7 @@ int main()
     // attach the socket
     if (bind(sd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1)
     {
-        perror("bind unsuccesfull");
+        perror("bind unsuccessful");
         return errno;
     }
 
@@ -50,53 +57,51 @@ int main()
         perror("server is full\n");
         return errno;
     }
-    signal(SIGINT, SIG_DFL);
+
+    // make the accept, in case of something return an error
+    client = accept(sd, (struct sockaddr *)&from, &length);
+    if (client < 0)
+    {
+        perror("accept error.\n");
+    }
+
+    // if the connection is made successfully we start the loop
     while (1)
     {
-        int client;
-        unsigned int length = sizeof(from);
-
         // clear the output stream
         fflush(stdout);
 
-        // make the accept, in case of something return an error
-        client = accept(sd, (struct sockaddr *)&from, &length);
-        if (client < 0)
-        {
-            perror("accept error.\n");
-            continue;
-        }
-        // if not,the connection is made succesfully and we clear the string
+        // if not, the connection is made successfully and we clear the string
         MessageFromClient.clear();
         MessageFromClient.resize(1024);
         int bytes_read = read(client, MessageFromClient.data(), MessageFromClient.size());
         if (bytes_read > 0)
         {
             MessageFromClient.resize(bytes_read); // Shrink to actual size
-            std::cout << "I recived:" << MessageFromClient << std::endl;
+            std::cout << "I received: " << MessageFromClient << std::endl;
         }
         else if (bytes_read < 0)
         {
             perror("read error");
             close(client);
-            continue;
+            break; // exit loop and wait for a new connection
         }
         else
         {
             std::cout << "client disconnected" << std::endl;
             close(client);
-            continue;
+            break; // exit loop and wait for a new connection
         }
         // test case
         MessageToClient = "hello " + MessageFromClient;
-        ssize_t bytes_written = write(client, MessageToClient.data(), MessageToClient.size());
+        size_t bytes_written = write(client, MessageToClient.data(), MessageToClient.size());
         if (bytes_written < 0)
         {
             perror("write error");
         }
         MessageToClient.clear(); // clear the message after sending it to client
-        close(client);
     }
+    close(client);
     close(sd);
     return 0;
 }
